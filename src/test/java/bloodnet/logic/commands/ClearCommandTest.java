bb1cc53e1773e39f417d7040c0e883399b5a5176
Tasty <alexius2111@gmail.com>
2025-10-12 09:23:16 +0800

    Add test coverage for command sessions

--- src/test/java/bloodnet/logic/commands/ClearCommandTest.java
+++ src/test/java/bloodnet/logic/commands/ClearCommandTest.java
@@ -2,9 +2,15 @@ package bloodnet.logic.commands;
 
 import static bloodnet.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static bloodnet.testutil.TypicalPersons.getTypicalAddressBook;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import bloodnet.logic.commands.commandsessions.CommandSession;
+import bloodnet.logic.commands.commandsessions.ConfirmationCommandSession;
+import bloodnet.logic.commands.exceptions.CommandException;
 import bloodnet.model.AddressBook;
 import bloodnet.model.Model;
 import bloodnet.model.ModelManager;
@@ -27,6 +33,20 @@ public class ClearCommandTest {
         expectedModel.setAddressBook(new AddressBook());
 
         assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void createSession_nullModel_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> (new ClearCommand()).createSession(null));
+    }
+
+    @Test
+    public void createSession_validModel_returnsConfirmationCommandSession() throws CommandException {
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        CommandSession session = (new ClearCommand()).createSession(model);
+
+        assertTrue(session instanceof ConfirmationCommandSession);
+        assertFalse(session.isDone());
     }
 
 }
--- src/test/java/bloodnet/logic/commands/EditCommandTest.java
+++ src/test/java/bloodnet/logic/commands/EditCommandTest.java
@@ -13,6 +13,7 @@ import static bloodnet.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static bloodnet.testutil.TypicalPersons.getTypicalAddressBook;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -20,6 +21,9 @@ import org.junit.jupiter.api.Test;
 import bloodnet.commons.core.index.Index;
 import bloodnet.logic.Messages;
 import bloodnet.logic.commands.EditCommand.EditPersonDescriptor;
+import bloodnet.logic.commands.commandsessions.CommandSession;
+import bloodnet.logic.commands.commandsessions.ConfirmationCommandSession;
+import bloodnet.logic.commands.exceptions.CommandException;
 import bloodnet.model.AddressBook;
 import bloodnet.model.Model;
 import bloodnet.model.ModelManager;
@@ -181,4 +185,21 @@ public class EditCommandTest {
         assertEquals(expected, editCommand.toString());
     }
 
+    @Test
+    public void createSession_nullModel_throwsNullPointerException() {
+        Command editCommand = new EditCommand(Index.fromZeroBased(0), new EditPersonDescriptor());
+
+        assertThrows(NullPointerException.class, () -> editCommand.createSession(null));
+    }
+
+    @Test
+    public void createSession_validModel_returnsConfirmationCommandSession() throws CommandException {
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        Command editCommand = new EditCommand(Index.fromZeroBased(0), new EditPersonDescriptor());
+
+        CommandSession session = editCommand.createSession(model);
+
+        assertTrue(session instanceof ConfirmationCommandSession);
+        assertFalse(session.isDone());
+    }
 }
