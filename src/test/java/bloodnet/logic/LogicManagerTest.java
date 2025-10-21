8a36fda1d468a3c448357d7f148d63a6fb42ca98
TastyVeggy <alexius2111@gmail.com>
2025-10-21 14:32:42 +0800

    LogicManagerTest: add test for getFilteredDonationRecordsList

--- src/test/java/bloodnet/logic/LogicManagerTest.java
+++ src/test/java/bloodnet/logic/LogicManagerTest.java
@@ -134,6 +134,11 @@ public class LogicManagerTest {
         assertThrows(UnsupportedOperationException.class, () -> logic.getFilteredPersonList().remove(0));
     }
 
+    @Test
+    public void getFilteredDonationRecordsList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, () -> logic.getFilteredDonationRecordList().remove(0));
+    }
+
     /**
      * Executes the command and confirms that
      * - no exceptions are thrown <br>
